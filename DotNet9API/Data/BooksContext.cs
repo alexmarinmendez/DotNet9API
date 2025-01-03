3a1c13a4664991d62ddafc28caffe28660c4b289
Alex Marín Méndez <alexmarinmendez@gmail.com>
2025-01-02 20:22:59 -0500

    feat: create BooksContext

---
+++ DotNet9API/Data/BooksContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNet9API.Data
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
