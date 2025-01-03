b68e3a89e92a34e87f8f09e9c991c8520a975095
Alex Marín Méndez <alexmarinmendez@gmail.com>
2025-01-02 19:47:43 -0500

    chore: config Scalar at Program.cs

--- DotNet9API/Program.cs
+++ DotNet9API/Program.cs
@@ -1,10 +1,15 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 var summaries = new[]
 {
