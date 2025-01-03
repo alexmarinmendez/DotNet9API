40e49c46a0755c69b82b1ada7d345574bc94a9d6
Alex Marín Méndez <alexmarinmendez@gmail.com>
2025-01-02 20:26:08 -0500

    chore: add DbContext service to Program.cs

--- DotNet9API/Program.cs
+++ DotNet9API/Program.cs
@@ -1,9 +1,12 @@
+using DotNet9API.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<BooksContext>(options => options.UseInMemoryDatabase("BooksDB"));
 
 var app = builder.Build();
 
