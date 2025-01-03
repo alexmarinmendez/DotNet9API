d0ddc01569e929022802e0aa3ee190b97018c8dd
Alex Marín Méndez <alexmarinmendez@gmail.com>
2025-01-02 20:10:15 -0500

    feat: add Book model

---
+++ DotNet9API/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNet9API.Models
+{
+    public class Book
+    {
+        public required Guid Id { get; init; }
+        public required string Title { get; set; }
+        public required string Isbn { get; init; }
+    }
+}
